f63fee436c35f384fc52c0ee13219c379968cbca
zachmae <zacharie.lawson@epitech.eu>
2023-01-20 16:46:55 +0100

    feat(front-web): :sparkles: creation of a basic page that can redirect to login and signup

--- front-web/src/views/HomeView.tsx
+++ front-web/src/views/HomeView.tsx
@@ -1,7 +1,30 @@
-const HomeView = () => (
-	<>
-		<h1>Home</h1>
-	</>
-);
+import { Image, Box, Center, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
+const HomeView = () => {
+	const navigate = useNavigate();
+	const handleSignUp = () => {
+		navigate('/signup');
+	};
+	const handleLogin = () => {
+		navigate('/login');
+	};
+
+	return (
+		<Box bgImage="pictures/background.png" backgroundSize="cover" h="calc(100vh)">
+			<Center>
+				<Image marginTop={312} src="pictures/logo.png" />
+			</Center>
+			<Center>
+				<Button marginTop={40} colorScheme="blue" size="lg" onClick={handleLogin}>
+					Log in
+				</Button>
+				<Button marginTop={40} marginLeft={100} colorScheme="blue" size="lg" onClick={handleSignUp}>
+					Sign up
+				</Button>
+			</Center>
+		</Box>
+	);
+};
 
 export default HomeView;

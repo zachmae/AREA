8013570378a8136ee546630e8b765cfc1f2776c7
ValentinDurieux <valentin.durieux@epitech.eu>
2023-01-22 02:06:18 +0100

    feat(front-web): added basic login and sign up forms

--- front-web/src/views/LoginView.tsx
+++ front-web/src/views/LoginView.tsx
@@ -1,7 +1,26 @@
+import { Button, Input } from '@chakra-ui/react'
+import PasswordInput from '../components/PasswordInput'
+
 const LoginView = () => (
-	<>
+	<div>
 		<h1>Login</h1>
-	</>
+		<form>
+			<label>
+    	    {/* <p>Email</p>
+    	    <input type="text" name="identifier" /> */}
+			<Input placeholder='Email' />
+    	  </label>
+    	  <label>
+    	      {/* <p>Password</p>
+    	      <input type="password" /> */}
+			  <PasswordInput />
+    	  </label>
+    	  <div>
+    	    {/* <button type="submit">Submit</button> */}
+			<Button colorScheme='blue'>Login</Button>
+    	  </div>
+    	</form>
+	</div>
 );
 
 export default LoginView;
--- front-web/src/views/SignUpView.tsx
+++ front-web/src/views/SignUpView.tsx
@@ -1,7 +1,26 @@
+import { Button, Input } from '@chakra-ui/react'
+import PasswordInput from '../components/PasswordInput'
+
 const SignUpView = () => (
-	<>
+	<div>
 		<h1>Sign up</h1>
-	</>
+		<form>
+			<label>
+    	    {/* <p>Email</p>
+    	    <input type="text" name="identifier" /> */}
+			<Input placeholder='Email' />
+    	  </label>
+    	  <label>
+    	      {/* <p>Password</p>
+    	      <input type="password" /> */}
+			  <PasswordInput />
+    	  </label>
+    	  <div>
+    	    {/* <button type="submit">Submit</button> */}
+			<Button colorScheme='blue'>Sign up</Button>
+    	  </div>
+    	</form>
+	</div>
 );
 
 export default SignUpView;

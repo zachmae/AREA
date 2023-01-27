490fd54262c3c5c8a470119d0ec4229839c55f83
zachmae <zacharie.lawson@epitech.eu>
2023-01-27 11:19:03 +0100

    chore(front icons and download): :lipstick: modifications des icons du site et correction du telechargement de l'apk

--- front-web/src/app/Router.tsx
+++ front-web/src/app/Router.tsx
@@ -3,7 +3,6 @@ import HomeView from '../views/HomeView';
 import LoginView from '../views/LoginView';
 import SignUpView from '../views/SignUpView';
 import PlaygroundView from '../views/PlaygroundView';
-import DownloadView from '../views/DownloadView';
 
 const Router = () => (
 	<BrowserRouter>
@@ -12,7 +11,6 @@ const Router = () => (
 			<Route path="/login" element={<LoginView />} />
 			<Route path="/signup" element={<SignUpView />} />
 			<Route path="/playground" element={<PlaygroundView />} />
-			<Route path="/client.apk" element={<DownloadView />} />
 			<Route path="*" element={<Navigate to="/" replace />} />
 		</Routes>
 	</BrowserRouter>

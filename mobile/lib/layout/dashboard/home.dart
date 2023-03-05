a3c9a2e9cad1784bebafb8e7dbd9f7887afe1a2d
Erwan Cariou <erwan1.cariou@epitech.eu>
2023-03-05 15:19:31 +0100

    feat(mobile): :sparkles: display the argument of area action/reaction on the area list and area logs page (wainting for backend to test)

--- mobile/lib/layout/dashboard/home.dart
+++ mobile/lib/layout/dashboard/home.dart
@@ -5,6 +5,8 @@
 ** Wrote by Erwan Cariou <erwan1.cariou@epitech.eu>
 */
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:area/model/flex_size.dart';
 import 'package:area/layout/load.dart';
@@ -31,9 +33,6 @@ class _HomePageState extends State<HomePage> {
         area[0] = {};
         switchList.add(false);
         for (var i = 0; i < value.length; i++) {
-          String dateString = value[i]['createdAt'];
-          DateTime date = DateTime.parse(dateString);
-          print(date);
           switchList.add(value[i]['active']);
           area[i + 1] = {
             'actionService': value[i]['serviceAct'],
@@ -44,6 +43,7 @@ class _HomePageState extends State<HomePage> {
             'reactionArgs': value[i]['reactionArgs'],
           };
         }
+        print(area[1]!['actionArgs']!);
         setState(() {
           first = false;
           isLoading = false;
@@ -81,13 +81,16 @@ class _HomePageState extends State<HomePage> {
                 children: <Widget> [
                 const SizedBox(height: 25),
                 for(int i = 1; i < area.length; i++)
-                  basicArea(context, i, actionService: area[i]!['actionService']!, actionAction: area[i]!['actionAction']!, reactionService: area[i]!['reactionService']!, reactionAction: area[i]!['reactionAction']!),
+                  basicArea(context, i),
             ])
           ),
     );
   }
 
-  Widget basicArea(BuildContext context, int index, {required String actionService, required String actionAction, required String reactionService, required String reactionAction}) {
+  Widget basicArea(BuildContext context, int index) {
+    Map<String, dynamic> actionArgs = jsonDecode(area[index]!['actionArgs']!);
+    Map<String, dynamic> reactionArgs = jsonDecode(area[index]!['reactionArgs']!);
+
     return Container(
         width: perWidth(context, 80),
         margin: const EdgeInsets.all(30.0),
@@ -102,23 +105,44 @@ class _HomePageState extends State<HomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(actionService, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                Text(area[index]!['actionService']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
                 const SizedBox(height: 10),
-                Text(actionAction, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                Text(area[index]!['actionAction']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                const SizedBox(height: 10),
               ],
             ),
+            const SizedBox(height: 5),
+            for(int i = 0; i < actionArgs.length; i++)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('${actionArgs.keys.elementAt(i)} : ', style: const TextStyle(fontSize: 15, color: Colors.black)),
+                  const SizedBox(height: 10),
+                  Text(actionArgs.values.elementAt(i), style: const TextStyle(fontSize: 15, color: Colors.black)),
+                ],
+              ),
             const SizedBox(height: 15),
             const Icon(Icons.arrow_downward, size: 30),
             const SizedBox(height: 15),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(reactionService, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                Text(area[index]!['reactionService']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
                 const SizedBox(height: 10),
-                Text(reactionAction, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                Text(area[index]!['reactionAction']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
               ],
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 5),
+            for(int i = 0; i < reactionArgs.length; i++)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('${reactionArgs.keys.elementAt(i)} : ', style: const TextStyle(fontSize: 15, color: Colors.black)),
+                  const SizedBox(height: 10),
+                  Text(reactionArgs.values.elementAt(i), style: const TextStyle(fontSize: 15, color: Colors.black)),
+                ],
+              ),
+            const SizedBox(height: 15),
             Container(
               width: perWidth(context, 90),
               decoration: const BoxDecoration(
@@ -159,8 +183,6 @@ class _HomePageState extends State<HomePage> {
                           first = true;
                         });
                       });
-                      /*area.remove(index);
-                      switchList.removeAt(index);*/
                     });
                     // Handle delete button press
                   },
--- mobile/lib/layout/dashboard/logs.dart
+++ mobile/lib/layout/dashboard/logs.dart
@@ -5,6 +5,8 @@
 ** Wrote by Erwan Cariou <erwan1.cariou@epitech.eu>
 */
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:area/model/flex_size.dart';
 import 'package:area/layout/load.dart';
@@ -78,12 +80,84 @@ class _LogsPageState extends State<LogsPage> {
               children: <Widget> [
                 const SizedBox(height: 25),
                 for(int i = 1; i < area.length; i++)
-                  basicArea(context, i, actionService: area[i]!['actionService']!, actionAction: area[i]!['actionAction']!, reactionService: area[i]!['reactionService']!, reactionAction: area[i]!['reactionAction']!),
+                  basicArea(context, i),
               ])
       ),
     );
   }
 
+  Widget basicArea(BuildContext context, int index) {
+    Map<String, dynamic> actionArgs = jsonDecode(area[index]!['actionArgs']!);
+    Map<String, dynamic> reactionArgs = jsonDecode(area[index]!['reactionArgs']!);
+
+    return Container(
+        width: perWidth(context, 80),
+        margin: const EdgeInsets.all(30.0),
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1,
+          ),
+        ),
+        child: Column(children: [
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(area[index]!['actionService']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+              const SizedBox(height: 10),
+              Text(area[index]!['actionAction']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+              const SizedBox(height: 10),
+            ],
+          ),
+          const SizedBox(height: 5),
+          for(int i = 0; i < actionArgs.length; i++)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('${actionArgs.keys.elementAt(i)} : ', style: const TextStyle(fontSize: 15, color: Colors.black)),
+                const SizedBox(height: 10),
+                Text(actionArgs.values.elementAt(i), style: const TextStyle(fontSize: 15, color: Colors.black)),
+              ],
+            ),
+          const SizedBox(height: 15),
+          const Icon(Icons.arrow_downward, size: 30),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(area[index]!['reactionService']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+              const SizedBox(height: 10),
+              Text(area[index]!['reactionAction']!, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+            ],
+          ),
+          const SizedBox(height: 5),
+          for(int i = 0; i < reactionArgs.length; i++)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('${reactionArgs.keys.elementAt(i)} : ', style: const TextStyle(fontSize: 15, color: Colors.black)),
+                const SizedBox(height: 10),
+                Text(reactionArgs.values.elementAt(i), style: const TextStyle(fontSize: 15, color: Colors.black)),
+              ],
+            ),
+          const SizedBox(height: 15),
+          Container(
+            width: perWidth(context, 90),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.black),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text('Created on : ${area[index]!['date']!}', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+            ],
+          ),);
+  }
+
+
+/*
   Widget basicArea(BuildContext context, int index, {required String actionService, required String actionAction, required String reactionService, required String reactionAction}) {
     return Container(
         width: perWidth(context, 80),
@@ -128,4 +202,5 @@ class _LogsPageState extends State<LogsPage> {
           Text('Created on : ${area[index]!['date']!}', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
         ],));
   }
+*/
 }
